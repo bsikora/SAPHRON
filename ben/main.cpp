4f6736800087ecd8d371dfe5edcd5cd908c8f8d6
Vikramjit Singh Rathee <vrathee@crcfe01.crc.nd.edu>
2016-09-13 11:22:26 -0400

    Vik update to main.cpp

--- ben/main.cpp
+++ ben/main.cpp
@@ -10,10 +10,18 @@
 #include <time.h>
 #include "../src/ForceFields/ForceFieldManager.h"
 #include "../src/ForceFields/LennardJonesTSFF.h"
+#include "../src/ForceFields/DSFFF.h"
 #include "../src/ForceFields/FENEFF.h"
 #include "../src/Moves/MoveManager.h"
+#include "../src/Moves/Move.h"
+#include "../src/Moves/InsertParticleMove.h"
+#include "../src/Moves/DeleteParticleMove.h"
+#include "../src/Moves/AcidReactionMove.h"
+#include "../src/Moves/AnnealChargeMove.h"
 #include "../src/Particles/Particle.h"
 #include "../src/Worlds/World.h"
+#include "../src/Worlds/WorldManager.h"
+#include "../src/Properties/Energy.h"
 
 #include "lammps.h"         // these are LAMMPS include files
 #include "input.h"
@@ -24,8 +32,43 @@ using namespace std;
 using namespace SAPHRON;
 using namespace LAMMPS_NS;
 
-int main(int narg, char **arg) 
+// forward declaration
+void saphronLoop(LAMMPS* &lmp, int &lammps, MoveManager &MM, WorldManager &WM, ForceFieldManager &ffm, ParticleList &Monomers, World &world); //const SAPHRON::MoveOverride &override
+int main(int narg, char **arg)
 {
+// Set up SAPHRON in the main function
+  ParticleList Monomers;
+  ForceFieldManager ffm;
+  SAPHRON::Particle poly("Polymer");
+  double rcut = 3.0;
+  World world(10.38498, 10.38498, 10.38498, rcut, 46732);
+  WorldManager WM;
+  WM.AddWorld(&world);
+  MoveManager MM (time(NULL));
+  //MoveOverride override = SAPHRON::MoveOveride::None;
+  //enum MoveOveride override = None;
+
+  LennardJonesTSFF lj(0.8, 1.0, {1.122});
+  FENEFF fene(1.0, 1.0, 30.0, 2.0);
+  // Electrostatics
+  // DSFFF dsf(0.1, 3.0);    
+  // ffm.AddNonBondedForceField("Monomer", "Monomer", dsf);
+  ffm.AddNonBondedForceField("Monomer", "Monomer", lj);
+  ffm.AddBondedForceField("Monomer", "Monomer", fene);
+  InsertParticleMove Ins({{"Monomer"}}, WM,20,false,time(NULL));
+  DeleteParticleMove Del({{"Monomer"}},false,time(NULL));
+  /*
+          AcidReactionMove AcidMv({{"Monomer"}}, {{"temp"}},WM,20,10,time(NULL));
+          AnnealChargeMove AnnMv({{"Polymer"}}, time(NULL));
+  */
+  MM.AddMove(&Ins);
+  MM.AddMove(&Del);
+  /*
+      MM.AddMove(&AcidMv);
+      MM.AddMove(&AnnMv);
+  */
+
+
   // setup MPI and various communicators
   // driver runs on all procs in MPI_COMM_WORLD
   // comm_lammps only has 1st P procs (could be all or any subset)
@@ -55,7 +98,6 @@ int main(int narg, char **arg)
   MPI_Comm_split(MPI_COMM_WORLD,lammps,0,&comm_lammps);
   
   // open LAMMPS input script
-
   FILE *fp;
   if (me == 0) {
     fp = fopen(arg[2],"r");
@@ -65,14 +107,20 @@ int main(int narg, char **arg)
     }
   }
 
+  // First time lammps run (lammps equilibration run)
   // run the input script thru LAMMPS one line at a time until end-of-file
   // driver proc 0 reads a line, Bcasts it to all procs
   // (could just send it to proc 0 of comm_lammps and let it Bcast)
   // all LAMMPS procs call input->one() on the line
   
+  // Create an instance of lammps and run for equilibration
   LAMMPS *lmp;
   if (lammps == 1) lmp = new LAMMPS(0,NULL,comm_lammps);
-
+  else
+  {
+  	std::cout<<"Couldn't make LAMMPS instance!"<<std::endl;
+  	exit(-1);
+  }
   int n;
   char line[1024];
   while (1) 
@@ -91,92 +139,120 @@ int main(int narg, char **arg)
 
   }
 
-  if (lammps == 1) // if lammps has right number of processors
+  int natoms = static_cast<int> (lmp->atom->natoms);
+  double *x = new double[3*natoms];
+  lammps_gather_atoms(lmp,"x",1,3,x);
+  Rand _rand(time(NULL));
+
+  // Intialize monomers
+  for(int i=0; i<natoms*3;i=i+3)
   {
-    for(int i=0; i<20;i++)
-      {// THIS FOR LOOP SIMULATES RUN (FOR E.G. RUN 100 IN LAMMPS WOULD CORRESPOND TO i<100 HERE), CAN LATER BE PUT AS ARGUMENT
-        lmp->input->one("run 1");
-        int natoms = static_cast<int> (lmp->atom->natoms); // get total numb atoms in simulation
-        double *x = new double[3*natoms]; // single array 3* times cause x,y and z
-        lammps_gather_atoms(lmp,"x",1,3,x); //  gathers all coords arranged by id , for type double, 3 elements per id and fill x
-        Rand _rand(time(NULL));   // seeding random number generator with time (every time you run it's gonna be diff)
-        // can replace with seed value
-        double* lammps_energy = ((double*) lammps_extract_variable(lmp,"tot","0")); //THIS WORKS
-        
-
-        cout << "*******************************************"<<endl;
-        cout <<"LAMMPS TOTAL ENERGY (ke+pe)"<<*lammps_energy<<endl; 
-        cout<<"TOTAL NO. OF ATOMS FROM LAMMPS "<<natoms<<endl;
-
-        //*******************************************************************************
-        // set rcut WORLD
-        double rcut = 3.0;   // ???
-
-        //Set simulation
-        World world(10.38498, 10.38498, 10.38498, rcut, 46732); //create a world simualar to lammps
-
-        SAPHRON::Particle poly("Polymer"); //Create the polymer 
-        ParticleList Monomers;
-
-        for(int i=0; i<natoms*3;i=i+3){
-          Monomers.push_back(new Particle({x[i],x[i+1],x[i+2]},{0.0,0.0,0.0}, "Monomer")); //Particle constructor
-        }
-  
-        for(int i=1; i<natoms-1;i++){
-          Monomers[i]->AddBondedNeighbor(Monomers[i+1]); // AddBondedNeighbor instead of AddNeighbor
-          Monomers[i]->AddBondedNeighbor(Monomers[i-1]);
-        }
-        Monomers[0]->AddBondedNeighbor(Monomers[1]);
-        Monomers[natoms-1]->AddBondedNeighbor(Monomers[natoms-2]);
-  
-        for(auto& c : Monomers)
-        {
-          poly.AddChild(c);
-        }
-  
-  
-        world.AddParticle(&poly);   // instead of PackWorld, packWorld was filling up the box
-        world.UpdateNeighborList();
-        //world.PackWorld({&poly}, {1.0}, 1, 0.00357); // polymer, number fractions, n = total number, density=resize box
-        // ABOVE VALUES IN PACKWORLD CORRESPOND SYSTEM IN LAMMPS
-
-        ForceFieldManager ffm;
-
-        LennardJonesTSFF lj(0.8, 1.0, {1.122}); // eps, sigma, cutoff. SIMILAR TO LAMMPS
-        FENEFF fene(1.0, 1.0, 30.0, 2.0); // eps, sigma, kspring, rmax, will get value from lammps. SIMILAR TO LAMMPS
-
-        //SAPHTRON forcefield
-        ffm.AddNonBondedForceField("Monomer", "Monomer", lj);
-        ffm.AddBondedForceField("Monomer", "Monomer", fene);
-
-        //Gather the energy from saphron
-        auto H1 = ffm.EvaluateEnergy(world);
-
-        cout<<"TOTAL ENERGY OF TEH WORLD FROM SAPHRON "<<H1.energy.total()<<endl;
-  
-        // TESTING COORDS saphron gets
-        for(auto& p : world)
-        {
-          for(auto& c1 : *p) {
-            Position pos = c1->GetPosition();
-            std::cout << pos[0] << " " << pos[1] << " " << pos[2] << std::endl;
-          }
-        }
-
-    //********************************************************************//
-
-        // PRINTING OUT VALUES OF x
-        for(int i = 0; i < 12; i++) {
-          cout << *(x + i) << endl;
-        }
-
-        delete [] x;
-        MPI_Barrier(MPI_COMM_WORLD);
-        MPI_Barrier(MPI_COMM_WORLD);
+    Monomers.push_back(new Particle({x[i],x[i+1],x[i+2]},{0.0,0.0,0.0}, "Monomer"));
   }
-}
 
+  for(int i=1; i<natoms-1;i++)
+  {
+    Monomers[i]->AddBondedNeighbor(Monomers[i+1]);
+    Monomers[i]->AddBondedNeighbor(Monomers[i-1]);
+  }
+
+  Monomers[0]->AddBondedNeighbor(Monomers[1]);
+  Monomers[natoms-1]->AddBondedNeighbor(Monomers[natoms-2]);
+
+  for(auto& c : Monomers)
+  {
+    poly.AddChild(c);
+  }
+  world.AddParticle(&poly);
+  world.SetChemicalPotential("Monomer", 10);
+
+  delete [] x;
+
+  //Make sure all threads are caught up to this point.
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  // WHILE LOOP (alternating between saphron and lammps)
+  while(1)
+  {
+    // Run saphron for M steps. Includes energy evaluation and create a lammps data file within this function
+    saphronLoop(lmp, lammps, MM, WM, ffm, Monomers, world); // SAPHRON::MoveOverride::None
+
+    // Delete instance of LAMMPS
+    if (lammps == 1) delete lmp;
+
+    // Create lammps instance
+      LAMMPS *lmp;
+      if (lammps == 1) lmp = new LAMMPS(0,NULL,comm_lammps);
+
+    // Read lammps input file (it will read the data file line also)
+      FILE *fp;
+      if (me == 0) {
+        fp = fopen(arg[2],"r");
+        if (fp == NULL) {
+          printf("ERROR: Could not open LAMMPS input script\n");
+          MPI_Abort(MPI_COMM_WORLD,1);
+        }
+      }
+
+      int n;
+      char line[1024];
+      while (1) 
+      {
+        if (me == 0) 
+        {
+          if (fgets(line,1024,fp) == NULL) n = 0;
+          else n = strlen(line) + 1;
+          if (n == 0) fclose(fp);
+        }
+
+        MPI_Bcast(&n,1,MPI_INT,0,MPI_COMM_WORLD);
+        if (n == 0) break;
+        MPI_Bcast(line,n,MPI_CHAR,0,MPI_COMM_WORLD);
+        if (lammps == 1) lmp->input->one(line);
+      }
+
+     // Run lammps for N steps, lammps_loop function deleted
+      lmp->input->one("run 10000"); // can be passed as an argument args[3] for example
+      Rand _rand(time(NULL));
+  }
   if (lammps == 1) delete lmp;
   // close down MPI
   MPI_Finalize();
+}
+
+
+// FUNCTION
+void saphronLoop(LAMMPS* &lmp, int &lammps, MoveManager &MM, WorldManager &WM, ForceFieldManager &ffm, ParticleList &Monomers, World &world){ // const SAPHRON::MoveOverride &override
+
+      // Gather coordinates from lammps instance
+      //if (lammps == 1)
+      //{
+        int natoms = static_cast<int> (lmp->atom->natoms);
+        double *x = new double[3*natoms];
+        lammps_gather_atoms(lmp,"x",1,3,x);
+        Rand _rand(time(NULL));
+      //}
+
+      // Set position of monomers 
+      int j = 0;
+      for(int i=0; i<natoms*3;i=i+3){
+            Monomers[j]->SetPosition({x[i],x[i+1],x[i+2]});
+            j++;
+      }
+
+      // Energy evaluation and setting
+      auto H1 = ffm.EvaluateEnergy(world);
+      world.SetEnergy(H1.energy);
+      world.UpdateNeighborList();
+      delete [] x;
+
+      // Perform moves for M steps ()
+      for(int i=0; i<10000;i=i++)
+      {
+            // picks either insert or delete
+        auto* move = MM.SelectRandomMove();
+        move->Perform(&WM,&ffm,MoveOverride::None);
+      }
+
+      //Write out datafile that is utilized by lammps input script (NEEDS COMPLETION)
 }
